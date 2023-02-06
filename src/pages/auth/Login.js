2d0fd76e0c367932aafedc5686cd4d58e30a68ce
Batuhan <batuhanors98@gmail.com>
2023-02-06 15:02:15 +0300

    first init

---
+++ src/pages/auth/Login.js
@@ -0,0 +1,13 @@
+import { View } from "react-native";
+import LoginForm from "../../components/LoginForm";
+import { loginStyles } from "./style/AuthStyles";
+
+const Login = () => {
+  return (
+    <View>
+      <LoginForm />
+    </View>
+  );
+};
+
+export default Login;
---
+++ src/pages/auth/style/AuthStyles.js
@@ -0,0 +1,40 @@
+import { StyleSheet } from "react-native";
+
+export const loginStyles = StyleSheet.create({
+  container: {
+    backgroundColor: "#FCFFE7",
+    alignItems: "center",
+    paddingTop: 40,
+  },
+  alignCenter: {
+    alignItems: "center",
+  },
+  loginHeader: {
+    marginTop: 2,
+    fontSize: 24,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  loginTxt: {
+    textAlign: "center",
+    marginTop: 54,
+    fontSize: 18,
+    marginBottom: 24,
+  },
+  loginForm: {
+    height: 500,
+    alignItems: "center",
+    width: "100%",
+    backgroundColor: "#76B852",
+  },
+  inputLabel: {
+    color: "white",
+    fontSize: 16,
+    marginBottom: 12,
+  },
+  inputText: {},
+  inputField: {
+    width: "80%",
+    margin: 32,
+  },
+});
